ffbc903def6fbfcde7c2712f7e8f9ffbd4c74fec
ivangarcia <ivangarciatecnico@gmail.com>
2018-07-12 02:37:37 +0200

    fixed mapping - App working

--- quitSmokingApp/application/src/main/java/com/everis/becaeoi/quitSmoking/quitSmokingApp/QuitSmokingAppApplication.java
+++ quitSmokingApp/application/src/main/java/com/everis/becaeoi/quitSmoking/quitSmokingApp/QuitSmokingAppApplication.java
@@ -2,8 +2,14 @@ package com.everis.becaeoi.quitSmoking.quitSmokingApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan({"com.everis.becaeoi.quitSmoking"})
+@EntityScan("com.everis.becaeoi.quitSmoking.persistence.entity")
+@EnableJpaRepositories("com.everis.becaeoi.quitSmoking.persistence.repository")
 public class QuitSmokingAppApplication {
 
 	public static void main(String[] args) {
